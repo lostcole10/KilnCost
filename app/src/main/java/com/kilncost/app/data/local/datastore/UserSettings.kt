4e962702cb567dbb9447031b27c4aa9277b68d85
cole <cole.urbanowski@ucalgary.ca>
2026-05-13 01:12:10 -0600

    object made

--- app/src/main/java/com/kilncost/app/data/local/datastore/UserSettings.kt
+++ app/src/main/java/com/kilncost/app/data/local/datastore/UserSettings.kt
@@ -1,4 +1,10 @@
 package com.kilncost.app.data.local.datastore
 
-class UserSettings {
-}
+data class UserSettings(
+    val timeCostPerHrCents: Int,
+    val profitMargin: Float,
+    val onboarded: Boolean,
+    val currencySymbol: String,
+    val dimensionUnit: String,
+    val weightUnit: String
+)
